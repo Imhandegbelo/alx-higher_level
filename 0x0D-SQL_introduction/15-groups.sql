10a6d2e41bf29b8be6d83fc6484b56f900a30c35
Imhandegbelo <abramgeo5@gmail.com>
2023-05-17 19:07:06 -0700

    update: task 12 - 16

---
+++ 0x0D-SQL_introduction/15-groups.sql
@@ -0,0 +1,4 @@
+-- lists the number of records with the same score in the table second_table
+-- List should be sorted by number of records in descending order
+-- Number of records for the score should be labelled as number
+SELECT score, COUNT(*) AS number FROM second_table GROUP BY score ORDER BY number DESC;
