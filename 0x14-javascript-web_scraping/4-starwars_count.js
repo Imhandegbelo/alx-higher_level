c09ce0575917ba959eee60b81f81d248aef9540c
Imhandegbelo <okogbe@outlook.com>
2023-07-26 22:04:38 -0700

    added 3-starwars_count

---
+++ 0x14-javascript-web_scraping/4-starwars_count.js
@@ -0,0 +1,14 @@
+#!/usr/bin/node
+//Prints the number of movies where the character "Wedge Antilles" is present
+
+const request = require('request');
+
+request(process.argv[2], function (err, res, body) {
+  if (err) {
+    console.error(err);
+  }
+  const nb = JSON.parse(body).results.filter((elem) => {
+    return elem.characters.filter((url) => { return url.includes('18'); }).length;
+  }).length;
+  console.log(nb);
+});
